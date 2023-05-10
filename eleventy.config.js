1f782269c93e2eb6c4dc1b9eee71b813839363f8
Felix Böttcher <62942125+felixboet@users.noreply.github.com>
2023-05-10 14:16:34 +0200

    picocss

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const sass = require("sass");
+const path = require("node:path");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -6,7 +8,23 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
 
+    // can be an async function
+    compile: function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", "node_modules/@picocss/pico/scss/"],
+      });
+
+      return (data) => {
+        return result.css;
+      };
+    },
+  });
   return {
     dir: {
       input: "src",
